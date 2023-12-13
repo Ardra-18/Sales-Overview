99709aafe0b4aaeee619a4c5bc24237466ecbad9
Ardra S M <ardra.s.m@ibm.com>
2023-12-13 18:37:22 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.casestudy.salesoverview05.zcsalesovp05.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
